597a30f5130bb2671e47f5d118122c6cff7ea1f9
sethv1 <sethv1@cs.uw.edu>
2017-01-27 13:43:13 +0100

    app now displays iframe with chloropleth

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_file
 import pandas as pd
 import pickle
+import os
 
 from functions import search_df
 from functions import generate_folium
@@ -9,6 +10,10 @@ with open("../datasets/parsed_filtered_df.pkl", "rb") as handle:
        main_df = pickle.load(handle)
 
 opinion_df = main_df[main_df['sentiment'] != 0]
+
+# Set up map directory
+if not os.path.exists('maps'):
+    os.makedirs('maps')
 
 app = Flask("opinion-map")
 
@@ -19,14 +24,26 @@ def serve_map(search_query):
     # stuff with pandas
 
     if search_query:
-        df = search_df(opinion_df, search_query.split(" "), search_exclusive=True)
+        df = search_df(opinion_df, search_query.split(" "), search_exclusive=False)
     else:
         df = opinion_df
 
     folium_map = generate_folium(df)
-    folium_map.save("map-test.html")
+    folium_map.save("maps/map-test-%s.html" % search_query)
 
-    return df.__repr__()
+#   return df.__repr__()
+    print(search_query)
+    return """
+    <iframe src="/map-test-%s.html" width="100%%" height="80%%">iframe debug text between tags</iframe>
+    """ % search_query
+
+@app.route('/map-test-<search_query>.html')
+def show_map(search_query):
+    # Get the folium map generated for this query
+    return send_file('maps/map-test-%s.html' % search_query)
+
+#TODO could serve some
+#@app.route('/<mapname>.html')
 
 if __name__ == '__main__':
     app.run()
